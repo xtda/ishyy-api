694896a028b9200ac93b0b947431a036ce7080df
Andrew Jackson <andrew@disvelop.net>
2018-08-11 16:31:55 +1000

    loss tax fix

--- app/models/stream_elements/overunder.rb
+++ app/models/stream_elements/overunder.rb
@@ -26,12 +26,12 @@ module StreamElements
         @player_name.give_points(win_amount - @amount)
         @message = "The dice rolled #{@dice[0]} and #{@dice[1]} (#{@choice}), Congratulations! #{@player_name.name}, you won #{win_amount} potatoes!"
         StreamElementsWrapper::Bot.new.message(@message)
-        Tax.new(@player_name.name, win_amount)
+        Tax.new(@player_name.name, win_amount).pay_tax
         PlayerAudit.log(@player_name.name, 'OVERUNDER', "Won #{win_amount} with a roll of #{@dice[0]} and #{@dice[1]} ")
       else
         @player_name.take_points(@amount)
         tax = (@amount * 0.25).to_i
-        Tax.new(@player_name.name, tax).pay_tax
+        MayorSystem.new('nil').add_funds(tax)
         @message = "Better luck next time #{@player_name.name} the dice rolled #{@dice[0]} and #{@dice[1]} (#{check_over_under}) you lost #{@amount} potatoes" 
         StreamElementsWrapper::Bot.new.message(@message)
         PlayerAudit.log(@player_name.name, 'OVERUNDER', "Lost #{@amount} with a roll of #{@dice[0]} and #{@dice[1]}")
